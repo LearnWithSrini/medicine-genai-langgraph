2e03bbfe47833ca85ca26e206b4e279d04c91d9a
srinivasulumunagalahmrc <127215904+srinivasulumunagalahmrc@users.noreply.github.com>
2025-11-09 22:43:19 +0000

    making smore chnages

--- backend/config.py
+++ backend/config.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
-PINECONE_API_KEY = os.getenv("PINECONE_API_KEY", "")
 
 LOCAL_SQLITE_PATH = os.getenv("LOCAL_SQLITE_PATH", "")
 
